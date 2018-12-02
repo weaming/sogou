98f363e066aa1f3ce7f65fe6005ff0235c74e22e
weaming <garden.yuen@gmail.com>
2018-12-02 18:22:32 +0800

    fix reference before declare

--- sogou.py
+++ sogou.py
@@ -3,28 +3,32 @@ import sys
 import requests
 import uuid
 import hashlib
+import simple_colors as C
 from simple_colors import *
 from objectify_json import ObjectifyJSON
 from request_data import headers, cookies, data as body
 
-version = '1.2'
+version = "1.3"
 
 
 def parse_args():
     import argparse
+
     parser = argparse.ArgumentParser()
-    parser.add_argument('text')
-    parser.add_argument('--gray', default=False, action='store_true', help='no colors')
+    parser.add_argument("text")
+    parser.add_argument(
+        "--gray", default=False, action="store_true", help="no colors"
+    )
     args = parser.parse_args()
     return args
 
 
 def get_text(text):
     text = text.strip()
-    if text.startswith('/') and os.path.isfile(text):
+    if text.startswith("/") and os.path.isfile(text):
         with open(text) as f:
             return f.read().strip()
-    if text == '-':
+    if text == "-":
         return sys.stdin.read().strip()
     return text
 
@@ -39,15 +43,15 @@ def cal_secret(data):
     """
     the key point
     """
-    key = 'front_9ee4f0a1102eee31d09b55e4d66931fd'
+    key = "front_9ee4f0a1102eee31d09b55e4d66931fd"
     a = f"{data['from']}{data['to']}{data['text']}{key}"
-    data['s'] = md5(a)
+    data["s"] = md5(a)
 
 
 def get_data(data, args):
     text = get_text(args.text)
-    data['text'] = text
-    data['uuid'] = str(uuid.uuid4())
+    data["text"] = text
+    data["uuid"] = str(uuid.uuid4())
     cal_secret(data)
     return data
 
@@ -58,34 +62,34 @@ def main():
     # disable colors for integrating with alfred
     if args.gray:
         yellow = green = lambda x: x
+    else:
+        green = C.green
+        yellow = C.yellow
 
-    api = 'https://translate.sogou.com/reventondc/translateV1'
+    api = "https://translate.sogou.com/reventondc/translateV1"
     req_data = get_data(body, args)
-    if os.getenv('DEBUG'):
+    if os.getenv("DEBUG"):
         print(req_data)
 
-    res = requests.post(
-        api,
-        headers=headers,
-        cookies=cookies,
-        data=req_data,
-    )
+    res = requests.post(api, headers=headers, cookies=cookies, data=req_data)
     data = res.json()
-    if res.status_code == 200 and data.get('status') == 0:
-        data = ObjectifyJSON(data['data'])
+    if res.status_code == 200 and data.get("status") == 0:
+        if os.getenv("DEBUG"):
+            print(data)
+        data = ObjectifyJSON(data["data"])
         bilingual = data.bilingual.data.list
 
         if os.getenv("SOGOU_SIMPLE"):
             print(data.translate.text)
             print(data.translate.dit)
         else:
-            print("{}: {}".format(green("  From"), data.translate['from']))
+            print("{}: {}".format(green("  From"), data.translate["from"]))
             print("{}: {}".format(green("    To"), data.translate.to))
             print("{}: {}".format(green("  Text"), data.translate.text))
             print("{}: {}".format(green("Result"), data.translate.dit))
 
             if bilingual:
-                print('')
+                print("")
                 for i, eg in enumerate(bilingual, start=1):
                     print(f"{yellow(i)}. {green(eg.summary.source)}")
                     print(f"   {eg.summary.target}\n")
@@ -96,5 +100,5 @@ def main():
         sys.exit(1)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
